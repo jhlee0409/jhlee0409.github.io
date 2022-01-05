230da901ac018d30c1e33d6d4db8da1572350e40
jack <relee6203@gmail.com>
2022-01-05 16:58:50 +0900

    Deploy Next.js to gh-pages

--- out/_next/static/Q09Jw4FQcTFc6c5uT4Mm8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/202b7dc706597123.css","static/chunks/pages/index-bc289e3a58d25ba7.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
