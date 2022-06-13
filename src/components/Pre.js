5fa6e58ad8551a2827eeb5497c7e40881075f91b
Jack <jhlee409@naver.com>
2022-06-14 03:10:12 +0900

    feat: copy code button base

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-
+import { copyButton } from "./pre.css";
 const Pre = ({ children }) => {
   const textInput = useRef(null);
   const [hovered, setHovered] = useState(false);
@@ -29,13 +29,9 @@ const Pre = ({ children }) => {
     >
       {hovered && (
         <button
-          aria-label="Copy code"
+          // aria-label="Copy code"
           type="button"
-          className={`absolute right-2 top-2 h-8 w-8 rounded border-2 bg-gray-700 p-1 dark:bg-gray-800 ${
-            copied
-              ? "border-green-400 focus:border-green-400 focus:outline-none"
-              : "border-gray-300"
-          }`}
+          className={copyButton}
           onClick={onCopy}
         >
           <svg
