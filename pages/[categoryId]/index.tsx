525c5a7e6b1167055acda93f1499f6a37bd75b2b
Jack <jhlee409@naver.com>
2022-06-22 23:05:27 +0900

    mdx: 리팩터링 초본

--- pages/[categoryId]/index.tsx
+++ pages/[categoryId]/index.tsx
@@ -1,4 +1,4 @@
-import { getPostCategory, getPostPaths, getPosts } from "@/helper/getPosts";
+import { getPostCategory, getPosts } from "@/helper/getPosts";
 import ArticleList from "@/components/articleList";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -14,7 +14,7 @@ export const getServerSideProps = async ({ params }: any) => {
   };
 };
 
-const ArticleListPage = ({ categories, data }: any) => {
+const ArticleListPage = ({ data }: any) => {
   const router = useRouter();
   return (
     <>
