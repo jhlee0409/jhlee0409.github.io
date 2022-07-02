87c25e98889ae53f74763868ea6db2b6b59499f0
Jack <jhlee409@naver.com>
2022-07-03 01:28:51 +0900

    feat: add link logo in footer

--- src/components/semantic/Footer/footer.css.ts
+++ src/components/semantic/Footer/footer.css.ts
@@ -1,11 +1,32 @@
-import { style } from "@vanilla-extract/css";
-
+import { globalStyle, style } from "@vanilla-extract/css";
 export const footerBox = style({
   width: "100%",
-  height: 100,
+  // height: 100,
   display: "flex",
-  justifyContent: "center",
+  alignItems: "center",
   padding: 20,
   fontSize: "0.8em",
   borderTop: "1px solid white",
+  flexDirection: "column",
+  gap: 20,
+});
+
+export const logoBox = style({
+  display: "flex",
+  gap: 20,
+  "@media": {
+    "screen and (max-width: 578px)": {
+      gap: 15,
+    },
+  },
+});
+
+globalStyle(`${logoBox} > a`, {
+  width: 40,
+  filter: "invert(1)",
+  "@media": {
+    "screen and (max-width: 578px)": {
+      width: 30,
+    },
+  },
 });
--- src/components/semantic/Footer/index.tsx
+++ src/components/semantic/Footer/index.tsx
@@ -1,8 +1,24 @@
-import { footerBox } from "./footer.css";
+import { footerBox, logoBox } from "./footer.css";
+import githubLogo from "@/assets/github.png";
+import linkedinLogo from "@/assets/linkedin.png";
+import Image from "next/image";
+
 const Footer = () => {
   return (
     <div className={footerBox}>
       <p>ⓒ 2022. JACK All rights reserved</p>
+      <div className={logoBox}>
+        <a href="https://github.com/jhlee0409" target="_blank" rel="noreferrer">
+          <Image src={githubLogo} alt="github" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jhlee0409/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image src={linkedinLogo} alt="linkedin" />
+        </a>
+      </div>
     </div>
   );
 };
