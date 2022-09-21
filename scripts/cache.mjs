46b3054ffd2c6c0224b05a7e9da7cbe738086e75
Jack <jhlee409@naver.com>
2022-09-22 01:15:44 +0900

    Deploy Next.js to gh-pages

---
+++ scripts/cache.mjs
@@ -0,0 +1,51 @@
+import path from "path";
+import fs from "fs";
+import matter from "gray-matter";
+const files = fs.readdirSync(path.join("posts"));
+
+export const getSearchedAllPosts = (folder) => {
+  const [big, sub] = folder.split("-");
+  const posts = fs.readdirSync(path.join(`posts/${big}/${sub}`));
+  const fileContents = posts.map((item) => {
+    const { data, content } = matter(
+      fs.readFileSync(path.join(`posts/${big}/${sub}/${item}`), "utf8")
+    );
+    return {
+      data: Object.assign(data, { mainCategory: big, subCategory: sub }),
+      title: data.title,
+      id: `${big}-${sub}`,
+    };
+  });
+
+  return fileContents;
+};
+
+export const getSearchedPost = () => {
+  const flatList = [];
+  const result = [];
+  for (const file of files) {
+    flatList.push(
+      ...fs.readdirSync(path.join(`posts/${file}`)).map((item) => {
+        return `${file}-${item}`;
+      })
+    );
+  }
+  for (const category of flatList) {
+    result.push(...getSearchedAllPosts(category));
+  }
+  return JSON.stringify(
+    result?.sort((a, b) => new Date(b.data.date) - new Date(a.data.date))
+  );
+};
+
+const fileContents = `export const posts = ${getSearchedPost()}`;
+try {
+  fs.readdirSync("out/cache");
+} catch (e) {
+  fs.mkdirSync("out/cache");
+}
+
+fs.writeFile("out/cache/data.js", fileContents, (err) => {
+  if (err) return console.log(err);
+  console.log("Posts cached.");
+});
